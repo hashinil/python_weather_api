7ced040a72eabcb7ea02c09b68aabf41f12bda62
Hashini <hashini.liyanage@gmail.com>
2023-08-01 21:13:14 +0200

    Weather data API day1

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask("__name__")
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
 
-app.run(debug=True)
+@app.route("/api/v1/<station>/<date>")
+def details(station, date):
+    temperature = 30
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
