6de648a0c30658f1d7dd326fdf322dcb570c8f1f
Hashini <hashini.liyanage@gmail.com>
2023-08-02 12:20:32 +0200

    Weather data API day4 -  add new functionalities

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Flask("__name__")
 
 stations = pd.read_csv("data/stations.txt", skiprows=17)
 stations = stations[['STAID','STANAME                                 ']]
+stations = stations[:10]
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
@@ -19,6 +20,23 @@ def details(station, date):
             "date": date,
             "temperature": temperature}
 
+
+@app.route("/api/v1/<station>")
+def station_details(station):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly_details(station, year):
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))]
+    result = result.to_dict(orient="records")
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
